c16e21095091c2df86f45862447ec409c56c75da
0Thomas1 <0Thomas1@cs.sfu.ca>
2025-10-05 15:18:40 -0700

    Implemented table and row structure

--- src/main.c
+++ src/main.c
@@ -2,12 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
+//SIZES
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+
+//
 typedef struct {
   char* buffer;
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
+
+//Row structure
+typedef struct{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+}Row;
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+//Row functions
+void serialize_row (Row * src, void* dest){
+    memcpy(&dest+ ID_OFFSET,&(src->id),ID_SIZE);
+    memcpy(&dest + USERNAME_OFFSET, &(src->username),USERNAME_SIZE);
+    memcpy(&dest + EMAIL_OFFSET, &(src->email),EMAIL_SIZE);
+}
+
+void deserialize_row (void* src, Row* dest){
+    memcpy(&(dest->id), &src + ID_OFFSET, ID_SIZE);
+    memcpy(&(dest->username), &src + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(dest->email), &src + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+//Table
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+  uint32_t num_rows;
+  void* pages[TABLE_MAX_PAGES];
+} Table;
 
 //command definition
 typedef enum {
@@ -15,7 +62,7 @@ typedef enum {
   META_COMMAND_UNRECOGNIZED_COMMAND
 } MetaCommandResult;
 
-typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT,PREPARE_SYNTAX_ERROR } PrepareResult;
 
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -30,6 +77,7 @@ typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
 
 typedef struct {
   StatementType type;
+  Row row_to_insert; // only use when inserting
 } Statement;
 
 void printPrompt (){printf("db > ");}
@@ -67,6 +115,14 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
     if(strncmp(input_buffer->buffer, "insert", 6) == 0){
         statement->type = STATEMENT_INSERT;
+        int args_assigned  = sscanf(
+            input_buffer->buffer,"insert %d %s %s",&(statement->row_to_insert.id),
+            statement->row_to_insert.username,
+            statement->row_to_insert.email
+        );
+        if(args_assigned > 3){
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if(strcmp(input_buffer->buffer, "select") == 0){
